b20ebd003c2e8933d47a60df6c939624c251a7ed
e <gongdesheng_lucky@126.com>
2019-07-12 12:11:39 +0800

    转存一下

---
+++ src/components/PageLoading/index.tsx
@@ -0,0 +1,27 @@
+import { ActivityIndicator } from 'antd-mobile';
+import React from 'react';
+
+export default () => (
+  <div
+    style={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: '100px',
+      height: '100px',
+    }}
+  >
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <ActivityIndicator animating size="large" />
+      <span style={{ marginTop: 8 }}>加载中...</span>
+    </div>
+  </div>
+);
